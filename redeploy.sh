e45da7531e5fd1ac4e1396b8afd6626abc25f38a
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-11 14:28:24 +0500

    update redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -3,8 +3,10 @@
 
 echo "hello shaxzod1"
 
+echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
+
 #1 serverba ula mishun "ssh " connect shudan darkor 
-ssh -i "/Users/shakhzod/Desktop/okok.pem" ec2-user@ec2-16-171-181-50.eu-north-1.compute.amazonaws.com <<EOF
+ssh -o StrictHostKeyChecking=no -i private_key ec2-user@ec2-16-171-181-50.eu-north-1.compute.amazonaws.com <<EOF
 
 
 # fileba daromadan darkor 
