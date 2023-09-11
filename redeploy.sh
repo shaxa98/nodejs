e35ff6480a80bc1c1a2018bc17afcd0234319da8
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-11 18:46:00 +0500

    update: actions

--- redeploy.sh
+++ redeploy.sh
@@ -2,7 +2,7 @@
 
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 #1 serverba ula mishun "ssh " connect shudan darkor 
-ssh -o StrictHostKeyChecking=no -i private_key ec2-user@13.53.170.229 <<EOF
+ssh -o StrictHostKeyChecking=no -i private_key $USER@$HOST <<EOF
 cd api
 git pull 
 npm install 
