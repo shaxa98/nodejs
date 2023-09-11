93f67bc86feeac50eea71e5c88c966685f7d2dbd
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-11 15:23:25 +0500

    clean my code

--- redeploy.sh
+++ redeploy.sh
@@ -1,31 +1,16 @@
 #!.bin/bash
-
-
 echo "hello shaxzod1"
-
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
-
 #1 serverba ula mishun "ssh " connect shudan darkor 
 ssh -o StrictHostKeyChecking=no -i private_key ec2-user@ec2-16-171-181-50.eu-north-1.compute.amazonaws.com <<EOF
-
-
-# fileba daromadan darkor 
 cd shaxzod/api
 pwd
 sudo su 
 git pull 
 whoami
-#curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-#sudo yum install nodejs -y
-#2 githubba deploy shudagi file "pull" mukunam 
 which npm 
-# npm pakeji nav boshad "npm i" usftanofka kadan darkor 
 npm install 
-#3 badi "pull" shudagesh Pm2 restart mukunam
-#npm i -g pm2 
- pm2 restart all
+pm2 restart all
 exit  
 
-#4 soni "pm2 ps " kada narmalni kor kaysasmi nemi tekshiri mukunam 
 EOF
-###S
