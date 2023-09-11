652adc6e27708e691edf4baa5e0c6f6a7f822f69
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-11 17:59:18 +0500

    update server

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,9 @@
 #!.bin/bash
 
-echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 #1 serverba ula mishun "ssh " connect shudan darkor 
-ssh -o StrictHostKeyChecking=no -i private_key ec2-user@ec2-16-171-181-50.eu-north-1.compute.amazonaws.com <<EOF
-cd shaxzod/api
-sudo su 
+ssh -o StrictHostKeyChecking=no -i private_key ec2-user@ec2-13-53-170-229.eu-north-1.compute.amazonaws.com <<EOF
+cd api
+
 git pull 
 npm install 
 pm2 restart all
