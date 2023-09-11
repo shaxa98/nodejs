89ba8f9970a3176833ada88d40ca6ac2208b1fd2
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-11 16:56:36 +0500

    update: script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 #!.bin/bash
-echo "hello shaxzod1"
+
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 #1 serverba ula mishun "ssh " connect shudan darkor 
 ssh -o StrictHostKeyChecking=no -i private_key ec2-user@ec2-16-171-181-50.eu-north-1.compute.amazonaws.com <<EOF
