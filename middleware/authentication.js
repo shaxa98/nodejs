1126a14068253ef2d9ac88648b2777217e730e75
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-13 14:11:46 +0500

    update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.use("/", (req, res, next) => {
-  const headerPassword = req.headers.password;
-  console.log("headerPassword: ", headerPassword);
-  if (headerPassword !== "1234567890") {
-    res.status(401).send("Parol xato");
-    return;
-  }
-  console.log("Login success");
-  next();
-});
+// router.use("/", (req, res, next) => {
+//   const headerPassword = req.headers.password;
+//   console.log("headerPassword: ", headerPassword);
+//   if (headerPassword !== "1234567890") {
+//     res.status(401).send("Parol xato");
+//     return;
+//   }
+//   console.log("Login success");
+//   next();
+// });
 
-module.exports = router;
+// module.exports = router;
--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@
 echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
 #1 serverba ula mishun "ssh " connect shudan darkor 
 ssh -o StrictHostKeyChecking=no -i private_key $USER@$HOST <<EOF
-cd api
+cd nodejs
 git pull 
 npm install 
 pm2 restart all
