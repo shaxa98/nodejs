d6cb4cbb9d26a817c2a00158066c14e51f149208
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-13 17:39:09 +0500

    update action

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
   res.send("Oops, something went wrong.");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2500;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
