606f812f28c519adced57b11743eb3b535852e36
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-13 13:48:40 +0500

    update mongo DB port

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use((err, req, res, next) => {
   res.send("Oops, something went wrong.");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
