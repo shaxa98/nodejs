e7817f7d5d1e36bf0f1ca476eb2ea5ca51d3a37b
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-13 14:50:29 +0500

    update auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userRoute = require("./routes/users");
 const accountRoute = require("./routes/accounts");
-const authMiddleware = require("./middleware/authentication");
+//const authMiddleware = require("./middleware/authentication");
 const rootRoute = require("./routes/root");
 const imageRoute = require("./routes/image");
 const healthRoute = require("./routes/health");
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Auth Router
-app.use(authMiddleware);
+//app.use(authMiddleware);
 
 // Routers
 app.use("/user", userRoute);
