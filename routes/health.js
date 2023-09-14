46ed1eade73143dd124048069fde74a67c70699c
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-14 16:05:01 +0500

    update: workflow

--- routes/health.js
+++ routes/health.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send(" OK  ");
+  res.send({ status: "ok" });
 });
 
 module.exports = router;
